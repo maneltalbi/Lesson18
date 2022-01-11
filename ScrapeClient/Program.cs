c58ba381aaa97196d0f14007cc588391b617262a
manel talbi <talbimanel20@gmail.com>
2022-01-11 15:03:10 +0100

    create ScrapeClient and ScrapeLibrary in the same sollution lesson18

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using ScrapeLibrary;
+
+namespace SrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+
+
+
+        }
+        private string GetWebpage(string url)
+        {
+
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+            content += "that's all folks!!!";
+            return content;
+
+        }
+    }
+}
